cfe10d2baed66465ef6aceec405e3b836ffca8f6
kamalyes <501893067@qq.com>
2023-03-10 10:52:37 +0800

    :docs: feat：调整测试脚本为Examples

--- custard/core/factory.py
+++ custard/core/factory.py
@@ -963,6 +963,13 @@ class MsHelper(object):
             oneself:
         Returns:
         Examples:
+            >>> single_dict = {"code": "200", "error": "", "message": "", "data": []}
+            >>> single_mixture = {"code": 200, "details": [{"a1": True, "a2": True}], "total_count": {"c1": 1, "c2": 5}}
+            >>> double_mixture = {"code": 200, "details": [{"a1": True, "a2": True}, {"b1": True, "b2": True}], "total_count": {"c1": 1, "c2": 5}}
+            >>> sd_ov = MsHelper.ov_init(single_dict)
+            >>> sm_ov = MsHelper.ov_init(single_mixture)
+            >>> dm_ov = MsHelper.ov_init(double_mixture)
+            >>> oneself_ov = MsHelper.ov_init(double_mixture, oneself=True)
         """
         return f'${{{key}}}' if oneself else ""
 
